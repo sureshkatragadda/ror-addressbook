49d173af4b6bbcbab3291d9b1c6419588180212d
sureshkatragadda <suresh@needle.com>
2015-06-07 22:19:57 -0600

    contact controller + mongoid config

---
+++ app/models/contact.rb
@@ -0,0 +1,8 @@
+class Contact
+  include Mongoid::Document
+  field :first_name, type: String
+  field :last_name, type: String
+  field :email, type: String
+  field :phone, type: String
+  field :address, type: String
+end
