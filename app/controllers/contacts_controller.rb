5145633c2a7d6dd8c7cbe05813f0b063af5c4325
sureshkatragadda <suresh@needle.com>
2015-06-13 16:42:58 -0600

    adding validations for contacts

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,12 +4,16 @@ class ContactsController < ApplicationController
 	end
 
 	def new
+	 @contact = Contact.new
 	end
 
 	def create
 		@contact = Contact.new(contact_params)
-		@contact.save
-  		redirect_to @contact
+		if @contact.save
+	    	redirect_to @contact
+		  else
+		    render 'new'
+		  end
 	end
 
 	def show
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -4,4 +4,11 @@ class Contact
   field :email, type: String
   field :phone, type: String
   field :address, type: String
+ 
+  validates :name, presence: true,
+                    length: { minimum: 3 }
+
+  validates :phone, presence: true,
+                    length: { minimum: 10 }
+
 end
