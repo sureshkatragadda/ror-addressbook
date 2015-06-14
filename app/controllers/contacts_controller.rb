9da9bf8756a4cf4e0ed9a93d8c801ebd21cf4feb
sureshkatragadda <suresh@needle.com>
2015-06-13 21:35:38 -0600

    added links for show, edit, delete and functionality + style updates

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -21,18 +21,28 @@ class ContactsController < ApplicationController
 	end
 
 	def edit
+		@contact = Contact.find(params[:id])
 	end
 
 	def update
+	 @contact = Contact.find(params[:id])
+
+	  if @contact.update(contact_params)
+	    redirect_to @contact
+	  else
+	    render 'edit'
+	  end
 	end
 
 	def destroy	
+		@contact = Contact.find(params[:id])
 		@contact.destroy
+		redirect_to contacts_path
 	end
 
 	private
 	  def contact_params
-	    params.require(:contacts).permit(:name, :phone, :email, :address)
+	    params.require(:contact).permit(:name, :phone, :email, :address)
 	  end
 
 end
