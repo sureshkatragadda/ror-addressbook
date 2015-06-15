e576e04633c6525279c10d192fe510e31ef8b39a
sureshkatragadda <suresh@needle.com>
2015-06-14 19:15:50 -0600

    adding ajax support + bootstrap, simple form gems + styles

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,7 +1,6 @@
 class ContactsController < ApplicationController
-	def index
-		 @contacts = Contact.all
-	end
+	before_action :all_contacts, only: [:index, :create, :update, :destroy]
+  	before_action :set_contacts, only: [:edit, :update, :destroy]
 
 	def new
 	 @contact = Contact.new
@@ -10,37 +9,28 @@ class ContactsController < ApplicationController
 	def create
 		@contact = Contact.new(contact_params)
 		if @contact.save
-	    	redirect_to @contact
 		  else
 		    render 'new'
 		  end
 	end
 
-	def show
-		@contact = Contact.find(params[:id])
-	end
-
-	def edit
-		@contact = Contact.find(params[:id])
-	end
-
 	def update
-	 @contact = Contact.find(params[:id])
-
-	  if @contact.update(contact_params)
-	    redirect_to @contact
-	  else
-	    render 'edit'
-	  end
+	  @contact.update_attributes(contact_params)
 	end
 
 	def destroy	
-		@contact = Contact.find(params[:id])
 		@contact.destroy
-		redirect_to contacts_path
 	end
 
 	private
+	  def all_contacts
+	      @contacts = Contact.all
+	  end
+
+	  def set_contacts
+	      @contact = Contact.find(params[:id])
+      end
+
 	  def contact_params
 	    params.require(:contact).permit(:name, :phone, :email, :address)
 	  end
